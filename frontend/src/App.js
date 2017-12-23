2059b3129ab27bae359888753ba97021b5210029
Jeanaica <Home@102348-SDG-JSuplido.egg.ph>
2017-12-24 02:10:43 +0800

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import LoginForm from '../src/components/LoginForm'
+
+class App extends Component {
+  render() {
+    return <LoginForm />
+  }
+}
+
+export default App
